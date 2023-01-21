acb03e7b22d38fbea6545d945cfad8f41901fdf6
anna <anna@rcbd.org>
2023-01-20 19:13:46 -0500

    Create React app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello team! Here is will be our UI</h1>
+    </div>
+  );
+}
+
+export default App;
