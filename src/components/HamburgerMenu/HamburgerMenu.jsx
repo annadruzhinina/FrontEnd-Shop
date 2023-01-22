c86ba6640981321920833b31cc5b245ca9c1d4d6
anna <anna@rcbd.org>
2023-01-22 15:44:29 -0500

    Complete Hamburger menu

--- src/components/HamburgerMenu/HamburgerMenu.jsx
+++ src/components/HamburgerMenu/HamburgerMenu.jsx
@@ -1,6 +1,44 @@
 import React from "react";
 import "./hamburgerMenu.css";
-
-export default function HamburgerMenu() {
-  return <div>HamburgerMenu</div>;
+import { useState } from "react";
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import { Link } from "react-router-dom";
+import { SidebarData } from "../SidebarData/SidebarData.jsx";
+import { IconContext } from "react-icons";
+function HamburgerMenu() {
+  const [sidebar, setSidebar] = useState(false);
+  const showSidebar = () => setSidebar(!sidebar);
+  return (
+    <>
+      <IconContext.Provider value={{ color: "#fff" }}>
+        <div className="humburger">
+          <Link to="#" className="humburger-menu">
+            <FaIcons.FaBars onClick={showSidebar} />
+          </Link>
+        </div>
+        <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
+          <ul className="nav-menu-items" onClick={showSidebar}>
+            <li className="humburger-toggle">
+              <Link to="#" className="humburger-menu">
+                <AiIcons.AiOutlineClose />
+              </Link>
+            </li>
+            {SidebarData.map((item, index) => {
+              return (
+                <li key={index} className={item.cName}>
+                  <Link to={item.path}>
+                    {item.icon}
+                    <span>{item.title}</span>
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </IconContext.Provider>
+    </>
+  );
 }
+
+export default HamburgerMenu;
