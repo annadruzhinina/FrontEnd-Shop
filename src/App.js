73984466e35c73aee9012857d3feec0a5b68891b
bryxnt1 <bryantperez56@gmail.com>
2023-01-23 10:14:51 -0500

    file name fix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./screens/Home/Home.jsx";
-import Navbar from "./components/Navbar/Navbar.jsx";
+import Navbar from "./components/NavBar/NavBar.jsx";
 import Signin from "./screens/Signin/Signin.jsx";
 import Order from "./screens/Order/Order.jsx";
 import Cart from "./screens/Cart/Cart.jsx";
