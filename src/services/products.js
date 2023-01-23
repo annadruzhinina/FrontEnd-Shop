3d8f69209904dfa63ac7928abb3eff9a20b88195
muhammadwarrad <moe_warrad@yahoo.com>
2023-01-23 16:45:59 -0500

    CRUD function for products

---
+++ src/services/products.js
@@ -0,0 +1,46 @@
+import api from "./apiConfig.js";
+
+export const getProducts = async () => {
+  try {
+    const response = await api.get("/products");
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const getProduct = async (id) => {
+  try {
+    const response = await api.get(`/products/${id}`);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const createProduct = async (ProductData) => {
+  try {
+    const response = await api.post("/products", ProductData);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const updateProduct = async (id, ProductData) => {
+  try {
+    const response = await api.put(`/products/${id}`, ProductData);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const deleteProduct = async (id) => {
+  try {
+    const response = await api.delete(`/products/${id}`);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
