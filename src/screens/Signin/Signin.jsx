cc4f59a9c7e20901e0a5dfbc125d6dff0482ed38
bryxnt1 <bryantperez56@gmail.com>
2023-01-23 11:00:30 -0500

    signin jsx update

--- src/screens/Signin/Signin.jsx
+++ src/screens/Signin/Signin.jsx
@@ -1,6 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import "./signin.css";
 
 export default function Signin() {
+  const [user, setUser] = useState ({
+    username: "",
+    password: "",
+    message: ""
+  });
+  const handleSubmit = (e) => {
+
+    e.preventDefault();
+
+    if(user.password===user.passwordConfirm) {
+      setUser({message:"Nice to see you again"})
+
+    } else {
+      setUser({message:"Wrong password please try again"})
+
+    }
+
+  }
+  const handleChange = (e) => {
+    if (e.target.id === "username") {
+      setUser ({
+        username: e.target.value,
+      
+      })
+    }
+  }
+
+
+
   return <div>Sign In</div>;
 }
