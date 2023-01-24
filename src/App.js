961cf4cd58fd91dac1010eb79e96bc0a699a21c8
kwonangela <kwonangelaa@gmail.com>
2023-01-24 14:39:03 -0500

    added useState and useEffect for product data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // Imports
 import "./App.css";
+import React, { useState, useEffect, createContext, useContext } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./screens/Home/Home.jsx";
 import Navbar from "./components/NavBar/NavBar.jsx";
@@ -8,7 +9,31 @@ import Order from "./screens/Order/Order.jsx";
 import Cart from "./screens/Cart/Cart.jsx";
 import Category from "./components/Category/Categories.jsx";
 
+export const productDataContext = React.createContext();
+
+
+
 function App() {
+  const [productData, setProductData] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/")
+    .then(res => res.json())
+    .then(data => {
+      let prodData = data.mao((product) => {
+        return {
+          "title": product.title,
+          "description": product.description,
+          "price": product.price,
+          "brand": product.brand,
+          "category": product.category,
+          "images": product.images
+        }
+      });
+      setProductData(prodData);
+    })
+  }, [])
+
   return (
     <div className="app">
       <Router>
