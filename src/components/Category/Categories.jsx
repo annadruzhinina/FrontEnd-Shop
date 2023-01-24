d6866d3303cfa61b9f341d6102199c46b14eb259
anna <anna@rcbd.org>
2023-01-24 11:06:31 -0500

    Last update

--- src/components/Category/Categories.jsx
+++ src/components/Category/Categories.jsx
@@ -49,8 +49,8 @@ const ProductList = ({ products }) => {
         <div className="product-list">
           {products.map((product) => (
             <div className="product-description-category">
-              {/* <h3>{product.title}</h3>
-              <p>{product.description}</p> */}
+              <h3>{product.title}</h3>
+              <p>{product.description}</p>
               <img
                 className="product-img"
                 src={product.images}
@@ -65,3 +65,23 @@ const ProductList = ({ products }) => {
 };
 
 export default Category;
+
+// return (
+//   <div className="category-img-container">
+//     <div className="container">
+//       <div className="product-list">
+//         {products.map((product) => (
+//           <div className="product-description-category">
+//             <h3>{product.title}</h3>
+//             <p>{product.description}</p>
+//             <img
+//               className="product-img"
+//               src={product.images}
+//               alt={product.title}
+//             />
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   </div>
+// );
