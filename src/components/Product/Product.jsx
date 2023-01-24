5a9d5d038dba119dd75cc36b90e5b6f0ff2297b5
anna <anna@rcbd.org>
2023-01-24 14:07:56 -0500

    Update logic in Category and Product

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,39 +1,26 @@
 import React from "react";
 import "./product.css";
 
-
-function Product({
-  title,
-  description,
-  price,
-  rating,
-  brand,
-  category,
-  images,
-}) {
-  return (
-    <div className="product">
-      <div className="product-description">
-        <p className="product-title">{title}</p>
-        <p>{description}</p>
-        <p>{brand}</p>
-        <p>{category}</p>
-        <p className="product-price">
-          <small>$</small>
-          <strong>{price}</strong>
-        </p>
-        <div className="product-rating">
-          {Array(rating)
-            .fill()
-            .map((_) => (
-              <p>⭐️</p>
-            ))}
+function Product(props) {
+  const products = props.products?.map((product) => {
+    return (
+      <div className="product" key={product._id}>
+        <div className="product-description">
+          <p className="product-title">{product.title}</p>
+          <p>{product.description}</p>
+          <p>{product.brand}</p>
+          <p>{product.category}</p>
+          <p className="product-price">
+            <small>$</small>
+            <strong>{product.price}</strong>
+          </p>
         </div>
+        <img src={product.images} alt="" />
+        <button className="btn-add">Add to basket</button>
       </div>
-      <img src={images} alt="" />
-      <button className="btn-add">Add to basket</button>
-    </div>
-  );
+    );
+  });
+  return <div>{products}</div>;
 }
 
 export default Product;
