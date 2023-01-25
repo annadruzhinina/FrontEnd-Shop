96e097113f62922f0e5d49652b9f455e7f3e2768
kwonangela <kwonangelaa@gmail.com>
2023-01-25 17:50:19 -0500

    some comments added

--- src/components/Category/Categories.jsx
+++ src/components/Category/Categories.jsx
@@ -1,7 +1,7 @@
+// Imports
 import React, { useState, useContext } from "react";
 import { productDataContext } from "../../App.js";
 import "../HamburgerMenu/hamburgerMenu.css";
-import { Link } from "react-router-dom";
 import "./categories.css";
 import Product from "../Product/Product.jsx";
 
@@ -9,21 +9,22 @@ const Category = () => {
   const [selectedCategory, setSelectedCategory] = useState(null);
   const [filteredProducts, setFilteredProducts] = useState([]);
 
+  // Applying use context to productDataContext and setting it to data
   const data = useContext(productDataContext);
-  // if (data.length === 0) return <h1>Loading ... </h1>
-  console.log(data);
 
+  // Mapping through each product category
   const uniqueCategories = [
     ...new Set(data.map((product) => product.category))
   ];
 
+  // Showing products from each category when clicked  
   const handleCategoryClick = (category) => {
     setSelectedCategory(category)
     setFilteredProducts(
       data.filter((product) => product.category === category)
     );
   };
-  // if (filteredProducts.length === 0) return <h1>Loading 2 ... </h1>
+
   return (
     <>
       <div className="category-container">
@@ -31,7 +32,8 @@ const Category = () => {
           <div className="container">
             <div className="category-nav-menu">
               {uniqueCategories.map((category, index) => (
-                <button key={index}
+                <button
+                  key={index}
                   className="category-link"
                   onClick={() => handleCategoryClick(category)}
                 >
@@ -47,4 +49,5 @@ const Category = () => {
   );          
 };
 
+// Export Category
 export default Category;
