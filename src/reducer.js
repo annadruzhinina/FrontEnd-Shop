de379f98133a18ed20bdac986d4555cd2922d744
jagerziel <fromnytosg@gmail.com>
2023-01-27 06:23:01 -0500

    Working on stateprovider and reducer comments

--- src/reducer.js
+++ src/reducer.js
@@ -1,26 +1,32 @@
+// Export initial state
 export const initialState = {
   basket: [],
 };
+
+// Reducer function
 function reducer(state, action) {
   console.log(action);
+  // Set switch case for Add to cart and remove from cart
   switch (action.type) {
     case "ADD_TO_BASKET":
-      //Logic for adding item to basket
-      //...state => state[0], state[1], state[2]
-      // state store all objects for React app which is created by useStateValue()
-      // basket - one of keys in state object
-      // basket: [...state.basket, action.item] =>
-      //        1. create new value with name basket in state object
-      //        2. Put there all items from state.basket (...state.basket)
-      //        3. Put there action.item.
-      //           action.item holds product value which we dispatched from Product.addToBasket function
+      /*
+      Logic for adding item to basket
+      ...state => state[0], state[1], state[2]
+      state store all objects for React app which is created by useStateValue()
+      basket - one of keys in state object
+      basket: [...state.basket, action.item] =>
+             1. create new value with name basket in state object
+             2. Put there all items from state.basket (...state.basket)
+             3. Put there action.item.
+                action.item holds product value which we dispatched from Product.addToBasket function
+      */
 
+      // Variable to include basket filtered items
       const addItems = state.basket.filter(function (item) {
         return item.title === action.item.title;
       });
 
-      console.log("Items found", addItems, addItems.length);
-
+      // Conditional statement for basket items
       if (addItems.length > 0) {
         const newBasket = state.basket.map(function (item) {
           if (item.title === action.item.title) {
@@ -28,7 +34,6 @@ function reducer(state, action) {
           }
           return item;
         });
-
         return {
           ...state,
           basket: newBasket,
@@ -40,14 +45,12 @@ function reducer(state, action) {
           basket: [...state.basket, action.item],
         };
       }
-
+      // Break switch case
       break;
     case "REMOVE_FROM_BASKET":
       const removeItems = state.basket.filter(function (item) {
         return item.title === action.item.title;
       });
-
-      console.log("Remove Items", removeItems);
 
       if (removeItems.length > 0 && removeItems[0].quantity > 1) {
         const newBasket = state.basket.map(function (item) {
